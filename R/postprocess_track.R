c9ff855539fceb27dddc0b9f09afb2cb267380b3
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-02-09 11:20:33 +0100

    make sure dttm format is preserved

--- R/postprocess_track.R
+++ R/postprocess_track.R
@@ -1,8 +1,17 @@
+#' Title
+#'
+#' @param meta
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
 postprocess_track <- function(meta, data) {
 
   # link laps to each trackpoint
   laps <- dim(meta)[1]
-  lap <- rep(NA, dim(data)[1])
+  lap <- rep(Sys.time(), dim(data)[1])
 
   for (i in 1:laps-1) {
     ind <- data$Time >= meta$Laps[i] & data$Time < meta$Laps[i+1]
