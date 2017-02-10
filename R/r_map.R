9e14c37cfa681c5653fa1b3c9e7eef2352f2d315
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-02-10 15:37:45 +0100

    init commit mapping

---
+++ R/r_map.R
@@ -0,0 +1,31 @@
+#' Title
+#'
+#' @param data
+#' @param t0
+#' @param t1
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+r_map <- function(data, t0, t1) {
+
+  if(missing(t0)) {
+    t0 <- min(data$Time)
+  }
+
+  if (missing(t1)) {
+    t1 <- max(data$Time)
+  }
+
+  # process data
+  data <- dplyr::filter(.data = data, Time >= t0 & Time <= t1)
+
+  m <- leaflet::leaflet() %>%
+    leaflet::addProviderTiles("CartoDB.Positron") %>%
+    leaflet::clearBounds() %>%
+    leaflet::addPolylines(data$LongitudeDegrees, data$LatitudeDegrees)
+
+  m
+}
