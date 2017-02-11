b555f2b8cc3b16cbdf3dbd39586d893b16412389
Are Edvardsen <biorakel@gmail.com>
2017-02-11 17:06:44 +0100

    pace in trackpoint data

--- R/postprocess_track.R
+++ R/postprocess_track.R
@@ -17,10 +17,17 @@ postprocess_track <- function(meta, data) {
     ind <- data$Time >= meta$Laps[i] & data$Time < meta$Laps[i+1]
     lap[ind] <- meta$Laps[i]
   }
+
   # set last lap link in data
   ind <- data$Time >= meta$Laps[laps]
   lap[ind] <- meta$Laps[laps]
-
   data <- tibble::add_column(data, Lap = lap)
+
+  # add pace
+  len <- length(data$Time)
+  delta_mins <- difftime(data$Time[2:len], data$Time[1:len-1], units = "mins")
+  delta_km <- (data$DistanceMeters[2:len]-data$DistanceMeters[1:len-1]) / 1000
+  Pace <- c(NA, delta_mins / delta_km)
+  data <- tibble::add_column(data, Pace = Pace)
 
 }
