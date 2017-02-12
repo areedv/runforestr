ab96ab4f21133071eb04dc67d8fa03c0b88cb7ac
Are Edvardsen <biorakel@gmail.com>
2017-02-12 23:12:27 +0100

    explicit refs

--- R/r_time.R
+++ R/r_time.R
@@ -26,7 +26,20 @@ r_time <- function(data, t0, t1, segments) {
   pace <- c(NA, runmed(na.omit(data$Pace), 3))
   data <- dplyr::mutate(data, Pace = pace)
 
-  second_y <- list(title = "", #elevation
+  # get config
+  if (!exists("conf")) {
+    conf <- yaml::yaml.load_file(system.file("rfr.yml", package = "runforestr"))
+  }
+
+  # pulse
+  first_y <- list(title = "", overlaying = "n", side = "left",
+                  zeroline = FALSE,
+                  #range = c(min(data$HeartRateBpm), max(data$HeartRateBpm)),
+                  range = c(conf$runner$pulseRest, conf$runner$pulseMax),
+                  showgrid = FALSE)
+
+  # elevation
+  second_y <- list(title = "",
                    overlaying = "y",
                    side = "right",
                    zeroline = FALSE,
@@ -34,28 +47,41 @@ r_time <- function(data, t0, t1, segments) {
                    range = c(min(data$FakeAltitudeMeters),
                              max(data$FakeAltitudeMeters) * 2),
                    showgrid = FALSE, showticklabels = FALSE)
-
-  third_y <- list(title = "", #pace
+  # pace
+  third_y <- list(title = "",
                   overlaying = "y", side = "right",
                   zeroline = FALSE, showgrid = FALSE, showticklabels = FALSE,
-                  range = c(10, 0))
-                  #autorange = "reversed", range = c(0, 10))
+                  range = c(9.8, 0))
 
-  p <- plotly::plot_ly(x = ~ data$Time) %>%
-    add_trace(y = ~ data$FakeAltitudeMeters, name = "Elevation",
+  p <- plotly::plot_ly(x = ~ data$Time, mode = "lines") %>%
+    # test with pulse zones
+    plotly::add_trace(y = ~ rep(147, length(data$Time)), name = "Z3", mode = "none",
+              type = "scatter", yaxis = "y1", fillcolor = "rgba(0, 255, 0, 0.3)",
+              fill = "tonexty", hoverinfo = "text") %>%
+    plotly::add_trace(y = ~ rep(160, length(data$Time)), name = "Z4", mode = "none",
+              type = "scatter", yaxis = "y1", fillcolor = "rgba(255, 255, 0, 0.3)",
+              fill = "tonexty", hoverinfo = "text") %>%
+    plotly::add_trace(y = ~ rep(170, length(data$Time)), name = "Z5", mode = "none",
+              type = "scatter", yaxis = "y1", fillcolor = "rgba(255, 0, 0, 0.3)",
+              fill = "tonexty", hoverinfo = "text") %>%
+    plotly::add_trace(y = ~ data$FakeAltitudeMeters, name = "Elevation",
               type = "scatter", mode = "none", fill ="tozeroy",
-              yaxis = "y2", fillcolor = "rgba(190, 190, 190, 1)",
+              yaxis = "y2", fillcolor = "rgba(190, 190, 190, 0.3)",
               text = paste("Alt:", data$AltitudeMeters),
               hoverinfo = "text") %>%
-    add_trace(y = ~ data$HeartRateBpm,
-              type = "scatter", mode = "line+markers", name = "HR",
+    plotly::add_trace(y = ~ data$HeartRateBpm,
+              type = "scatter", mode = "none", name = "HR",
+              line = list(color = "rgb(0, 0, 0)", dash = "dot"),
+              yaxis = "y1", fill = "tozeroy", fillcolor = "rgba(255, 255, 255, 1)",
               text = paste("HR:", data$HeartRateBpm),
               hoverinfo = "text") %>%
-    add_trace(y = ~ data$Pace, name = "Pace", type = "scatter", mode = "line",
+
+    plotly::add_trace(y = ~ data$Pace, name = "Pace", type = "scatter", mode = "line",
               yaxis = "y3",
               text = paste("Pace:", as.difftime(data$Pace, units = "mins")),
               hoverinfo = "text") %>%
-    layout(yaxis2 = second_y, yaxis3 = third_y, legend = list(orientation = "h"))
+    plotly::layout(yaxis = first_y, yaxis2 = second_y, yaxis3 = third_y,
+           legend = list(orientation = "h"))
 
   p
 
