c38d1327cda7a4122d3d2a9934e08d4386dc1df7
Are Edvardsen <biorakel@gmail.com>
2017-02-12 13:48:38 +0100

    testing

--- R/r_time.R
+++ R/r_time.R
@@ -12,10 +12,15 @@
 r_time <- function(data, t0, t1, segments) {
 
   # for nicer plot:
-  # smooth and force zero altitude below sealevel
+  # smooth and fake altitude if obs below sealevel
   alt <- runmed(data$AltitudeMeters, 11)
-  alt[alt <= 0] <- 0
   data <- dplyr::mutate(data, AltitudeMeters = alt)
+  minAlt <- min(data$AltitudeMeters)
+  if ( minAlt < 0) {
+    alt <- alt - minAlt
+  }
+  data <- dplyr::mutate(data, FakeAltitudeMeters = alt)
+
 
   # smooth Pace
   pace <- c(NA, runmed(na.omit(data$Pace), 3))
@@ -25,7 +30,9 @@ r_time <- function(data, t0, t1, segments) {
                    overlaying = "y",
                    side = "right",
                    zeroline = FALSE,
-                   rangemode = "tozero",
+                   # keep at lower half of plot
+                   range = c(min(data$FakeAltitudeMeters),
+                             max(data$FakeAltitudeMeters) * 2),
                    showgrid = FALSE, showticklabels = FALSE)
 
   third_y <- list(title = "", #pace
@@ -35,16 +42,19 @@ r_time <- function(data, t0, t1, segments) {
                   #autorange = "reversed", range = c(0, 10))
 
   p <- plotly::plot_ly(x = ~ data$Time) %>%
-    add_trace(y = ~ data$AltitudeMeters, name = "Elevation",
+    add_trace(y = ~ data$FakeAltitudeMeters, name = "Elevation",
               type = "scatter", mode = "none", fill ="tozeroy",
-              yaxis = "y2", fillcolor = "rgba(190, 190, 190, 0.3)",
-              alpha = 1) %>%
+              yaxis = "y2", fillcolor = "rgba(190, 190, 190, 1)",
+              text = paste("Alt:", data$AltitudeMeters),
+              hoverinfo = "text") %>%
     add_trace(y = ~ data$HeartRateBpm,
               type = "scatter", mode = "line+markers", name = "HR",
-              text = paste("HR:", data$HeartRateBpm)) %>%
+              text = paste("HR:", data$HeartRateBpm),
+              hoverinfo = "text") %>%
     add_trace(y = ~ data$Pace, name = "Pace", type = "scatter", mode = "line",
               yaxis = "y3",
-              text = paste("Pace:", as.difftime(data$Pace, units = "mins"))) %>%
+              text = paste("Pace:", as.difftime(data$Pace, units = "mins")),
+              hoverinfo = "text") %>%
     layout(yaxis2 = second_y, yaxis3 = third_y, legend = list(orientation = "h"))
 
   p
