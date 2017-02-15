8f3adba4c710980bd2327ebd4a0bf03b838a1ca7
Are Edvardsen <biorakel@gmail.com>
2017-02-15 18:56:23 +0100

    working version

--- R/make_intesity_distribution.R
+++ R/make_intesity_distribution.R
@@ -21,34 +21,35 @@ make_intesity_distribution <- function(laps, tp_pulse, tp_time,
   }
 
   # make empty df for intensity distribution
-  n <- length(pulse_zones)
+  n <- length(pulse_zones) - 1
   idist <- as.data.frame(matrix(ncol = n))
+  ireldist <- idist
 
   # a tibble of data including timediff, last obs set to zero duration
   n <- length(tp_pulse)
   d <- as.numeric(difftime(tp_time[2:n], tp_time[1:(n - 1)], units = "secs"))
-  print(length(d))
   data <- tibble::tibble(pulse=tp_pulse, time=tp_time,
                         duration = c(d, 0))
 
   # iterate over laps
-  for (i in 1:length(laps) - 1) {
+  for (i in 1:(length(laps) - 1)) {
 
     # this lap
     lap <- dplyr::filter(data, time >= laps[i] & time < laps[i + 1])
 
     # iterate zones
     it <- vector()
-    for (j in 1:length(pulse_zones) - 1) {
+    for (j in 1:(length(pulse_zones) - 1)) {
       ind <- lap$pulse >= pulse_zones[j] & lap$pulse < pulse_zones[j + 1]
       it[j] <- sum(lap$duration[ind])
     }
     idist <- rbind(idist, it)
+    ireldist <- rbind(ireldist, (it / sum(it)))
   }
 
   # remove the first empty (NA) entry from creation...
   n <- dim(idist)[1]
   idist <- idist[2:n,]
 
-  idist <- tibble::as_tibble(idist)
+  list(idist = tibble::as_tibble(idist), ireldist = tibble::as_tibble(ireldist))
 }
