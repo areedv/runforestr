cbd4987156740a0dcd9bb82123c623f873528207
Are Edvardsen <biorakel@gmail.com>
2017-03-23 20:53:56 +0100

    init commit of parsing tcx data at activity level

---
+++ R/parse_garmin_tcx_activity.R
@@ -0,0 +1,42 @@
+#' Title
+#'
+#' @param tcx_nodeset
+#' @param ns
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+parse_garmin_tcx_activity <- function(tcx_nodeset, ns) {
+
+  ActivityIdXPath <- "//d1:Id"
+  SportXPath <- "//@Sport"
+  DeviceIdXPath <- "//d1:Creator/d1:UnitId"
+  DeviceNameXPath <- "//d1:Creator/d1:Name"
+
+  date_time_format <- "%FT%X" #2017-02-02T16:01:09.000Z
+
+  Id <- tcx_nodeset %>%
+    xml2::xml_find_first(ActivityIdXPath, ns) %>%
+    xml2::xml_text()
+
+  DateTime <- Id %>%
+    strptime(., date_time_format, tz = "GMT") %>%
+    as.POSIXct()
+
+  Sport <- tcx_nodeset %>%
+    xml2::xml_find_first(SportXPath, ns) %>%
+    xml2::xml_text()
+
+  DeviceId <- tcx_nodeset %>%
+    xml2::xml_find_first(DeviceIdXPath, ns) %>%
+    xml2::xml_text()
+
+  DeviceName <- tcx_nodeset %>%
+    xml2::xml_find_first(DeviceNameXPath, ns) %>%
+    xml2::xml_text()
+
+  tibble::tibble(Id = Id, Sport = Sport, DateTime = DateTime,
+                 DeviceId = DeviceId, DeviceName = DeviceName)
+}
