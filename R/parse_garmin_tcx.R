c8bd7a62f8a94a2f554a4a2e84489937c623a4ca
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-03-24 14:54:11 +0100

    testing...

--- R/parse_garmin_tcx.R
+++ R/parse_garmin_tcx.R
@@ -8,7 +8,8 @@
 #'
 #' @param tcx_file
 #'
-#' @return list containg tibbles 'meta' and 'data'
+#' @return  A list containg tibbles 'acti', meta' and 'data' in addition to the
+#' file name of the actual input data and its md5 checksum
 #' @export
 #'
 #' @examples
@@ -42,9 +43,9 @@ parse_garmin_tcx <- function(tcx_file) {
       meta <- parse_garmin_tcx_metadata(sub_doc, ns)
       data <- parse_garmin_tcx_trackpoint(sub_doc, ns)
     } else {
-      acti <- cbind(parse_garmin_tcx_activity(sub_doc, ns), acti)
-      meta <- cbind(parse_garmin_tcx_metadata(sub_doc, ns), meta)
-      data <- cbind(parse_garmin_tcx_trackpoint(sub_doc, ns), data)
+      acti <- rbind(parse_garmin_tcx_activity(sub_doc, ns), acti)
+      meta <- rbind(parse_garmin_tcx_metadata(sub_doc, ns), meta)
+      data <- rbind(parse_garmin_tcx_trackpoint(sub_doc, ns), data)
 
     }
   }
@@ -58,6 +59,9 @@ parse_garmin_tcx <- function(tcx_file) {
   # for debugging. To be removed
   print(paste("Stop parsing tcx at", Sys.time()))
 
-  list(filename=basename(tcx_file), md5sum=md5sum, acti=acti, meta=meta,
-       data=data)
+  # list(filename=basename(tcx_file), md5sum=md5sum, acti=acti, meta=meta,
+  #     data=data)
+  tibble::tibble(filename=basename(tcx_file), md5sum=md5sum,
+                 acti=list(acti),
+                 meta=list(meta), data=list(data))
 }
