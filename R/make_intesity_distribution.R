a0e8c9e7721cf5976541608a4e41ab9e2e65f848
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-06-30 14:19:31 +0200

    optionally add dummy data to represent all pulse zones

--- R/make_intesity_distribution.R
+++ R/make_intesity_distribution.R
@@ -6,13 +6,19 @@
 #' @param pulse_zones vector of pulse zone limits (bpm). For lower limits
 #' endpoints are included, not for upper limits, _e.g._ the vector
 #' c(40, 60, 80) equals intervalls [40, 60>, [60, 80>
+#' @param use_empty_intensities logical defining if all pulse zones (as defined
+#' in \emph{pulse_zones} are to be
+#' provided in the distribution regardless what may be represented in the data
+#' (as defined in \emph{pulse}). If TRUE dummy data with zero duration are
+#' added for each lap (as defined in \emph{laps}) and pulse zone
 #'
 #' @return A (grouped) data frame of durations (in seconds) grouped by lap(s)
 #' and intesity zone
 #' @export
 #'
 #' @examples
-make_intesity_distribution <- function(laps, pulse, time, pulse_zones) {
+make_intesity_distribution <- function(laps, pulse, time, pulse_zones,
+                                       use_empty_intensities = FALSE) {
 
   if (length(laps) < 2) {
     stop("'laps' must contain at least two points in time")
@@ -20,6 +26,17 @@ make_intesity_distribution <- function(laps, pulse, time, pulse_zones) {
 
   if (length(pulse) != length(time)) {
     stop("Parameters 'tp_puls' and 'time' must be of equal length")
+  }
+
+  # use all zones, regardless of what may be present in data
+  # dummy data must provide zero duration
+  if (use_empty_intensities) {
+    for (i in 1:(length(pulse_zones) - 1)) {
+      for (j in 1:(length(laps) - 1)) {
+        pulse <- c(rep(pulse_zones[i], 2), pulse)
+        time <- c(rep(laps[j], 2), time)
+      }
+    }
   }
 
   # replace missing pulse observations by nearest value
