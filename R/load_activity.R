d7fd60070c863d7538b3169901d009f962f4eda9
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-07-05 14:59:55 +0200

    load also from dropbox

--- R/load_activity.R
+++ R/load_activity.R
@@ -7,10 +7,21 @@
 
 load_activity <- function() {
 
-  filename <- paste0(conf$store$local$path, conf$store$filename)
+  dbFilePath <- file.path(conf$store$dropbox$path, conf$store$filename)
+  tempFilePath <- file.path(tempdir(), conf$store$filename)
 
-  if (file.exists(filename)) {
-    dat <- readRDS(filename)
+  if (conf$store$dropbox$use) {
+    rdrop2::drop_auth()
+    if (rdrop2::drop_exists(path = dbFilePath)) {
+      rdrop2::drop_get(path = dbFilePath, local_file = tempFilePath,
+                       overwrite = TRUE)
+    }
+  }
+
+  filename <- file.path(conf$store$local$path, conf$store$filename)
+
+  if (file.exists(tempFilePath)) {
+    dat <- readRDS(tempFilePath)
     return(dat)
   } else {
     return(NULL)
