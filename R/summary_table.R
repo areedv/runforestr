4923331d50a3f24683a516c3c86f342566a90ec1
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-07-12 14:58:10 +0200

    correcting errors and adding sort and better formatting

--- R/summary_table.R
+++ R/summary_table.R
@@ -8,12 +8,22 @@
 #' @examples
 
 summary_table <- function(data) {
-  s1 <- d$data %>%
+
+  # convert pace to mins:secs
+  f <- function(decimal_mins) {
+    as.character(decimal_mins * 60) %>%
+      strptime(format = "%s") %>%
+      format("%M:%S")
+  }
+
+  s1 <- data$data %>%
     dplyr::group_by(ActivityId) %>%
     summarise(km=max(DistanceMeters/1000), maxHR=max(HeartRateBpm, na.rm=TRUE),
               mspeed=mean(Speed, na.rm=TRUE), mpace=mean(Pace, na.rm=TRUE))
-  s <- dplyr::inner_join(d$acti, s1, by="ActivityId")
+  s <- dplyr::inner_join(data$acti, s1, by="ActivityId")
 
-  tibble(Type=s$Sport, Time=strftime(s$DateTime, format="%F %R"), km=s$km,
-         maxHR=s$maxHR, mSpeed=s$mspeed, mPace=s$mpace)
+  dplyr::tibble(Type=s$Sport, Time=strftime(s$DateTime, format="%F %R"),
+                km=round(s$km, digits = 1), maxHR=s$maxHR,
+                mSpeed=round(s$mspeed, digits = 1),
+                mPace=f(s$mpace)) %>% arrange(desc(Time))
 }
