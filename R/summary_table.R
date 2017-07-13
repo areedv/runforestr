dcff1c4842e49bf39c5f9d56b1dbb2766896dc0c
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-07-13 15:11:00 +0200

    some clean up

--- R/summary_table.R
+++ R/summary_table.R
@@ -7,23 +7,16 @@
 #'
 #' @examples
 
-summary_table <- function(data) {
+summary_table <- function(dat) {
 
-  # convert pace to mins:secs
-  f <- function(decimal_mins) {
-    as.character(decimal_mins * 60) %>%
-      strptime(format = "%s") %>%
-      format("%M:%S")
-  }
-
-  s1 <- data$data %>%
+  s <- dat$data %>%
     dplyr::group_by(ActivityId) %>%
-    summarise(km=max(DistanceMeters/1000), maxHR=max(HeartRateBpm, na.rm=TRUE),
-              mspeed=mean(Speed, na.rm=TRUE), mpace=mean(Pace, na.rm=TRUE))
-  s <- dplyr::inner_join(data$acti, s1, by="ActivityId")
+    summarise(km = max(DistanceMeters/1000),
+              dur = as.numeric(difftime(max(Time), min(Time),
+                                        units = "mins")),
+              maxHR = max(HeartRateBpm, na.rm=TRUE),
+              mspeed = mean(Speed, na.rm=TRUE),
+              mpace = mean(Pace, na.rm=TRUE))
 
-  dplyr::tibble(Type=s$Sport, Time=strftime(s$DateTime, format="%F %R"),
-                km=round(s$km, digits = 1), maxHR=s$maxHR,
-                mSpeed=round(s$mspeed, digits = 1),
-                mPace=f(s$mpace)) %>% arrange(desc(Time))
+  dplyr::inner_join(dat$acti, s, by="ActivityId")
 }
